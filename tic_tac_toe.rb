8d906370ac769ae6e8cc464cebd3349c41c2df4d
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-02-12 18:58:37 -0400

    fill players by grid variable

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,6 +11,8 @@ class TicTacToe < Gosu::Window
     @line = @square*3
     @half_line = @line/2
     @half_square = @square/2
+
+    @grid = [[1, 2, 1], [2, 1, 2], [1, 2, 1]]
   end
 
   def offset_x(length)
@@ -24,15 +26,30 @@ class TicTacToe < Gosu::Window
   def draw
     draw_rect(0, 0, 800, 600, Gosu::Color::WHITE)
     draw_grid
+
+    @grid.each_index do |y|
+      @grid[y].each_index do |x|
+        if @grid[y][x] == 1
+          draw_player1(x, y)
+        elsif @grid[y][x] == 2
+          draw_player2(x, y)
+        end
+      end
+    end
+    
     draw_player1(offset_x(-@square), offset_y(@square))
     draw_player2(@center_x, offset_y(@square))
   end
 
-  def draw_player1(center_x, center_y)
+  def draw_player1(x, y)
+    center_x = @center_x-@half_line+@square*x+@half_square
+    center_y = @center_y-@half_line+@square*y+@half_square
     draw_rect(center_x-30, center_y-30, 60, 60, Gosu::Color::BLACK)
   end
 
-  def draw_player2(center_x, center_y)
+  def draw_player2(x, y)
+    center_x = @center_x-@half_line+@square*x+@half_square
+    center_y = @center_y-@half_line+@square*y+@half_square
     draw_triangle(center_x, center_y-30, Gosu::Color::BLACK,
                   center_x-30, center_y+30, Gosu::Color::BLACK,
                   center_x+30, center_y+30, Gosu::Color::BLACK)
