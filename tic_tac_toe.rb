1164b7fb4a78c6d4b570d7e126aa2b6b7d80022d
Evandro Murilo <evandro@eficienciafiscal.com.br>
2024-02-12 21:16:04 -0400

    extract Grid class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,17 +1,13 @@
 require 'gosu'
 
-class TicTacToe < Gosu::Window
-  def initialize
-    super(800, 600)
-    self.caption = 'Tic Tac Toe'
-
-    @center_x = 800/2
-    @center_y = 600/2
+class Grid
+  def initialize(w, h)
+    @center_x = w/2
+    @center_y = h/2
     @square = 120
     @line = @square*3
     @half_line = @line/2
     @half_square = @square/2
-
     @grid = [[1, 2, 1], [2, 1, 2], [1, 2, 1]]
   end
 
@@ -23,22 +19,7 @@ class TicTacToe < Gosu::Window
     @center_y + length
   end
 
-  def draw
-    draw_rect(0, 0, 800, 600, Gosu::Color::WHITE)
-    draw_grid
-
-    @grid.each_index do |y|
-      @grid[y].each_index do |x|
-        if @grid[y][x] == 1
-          draw_player1(x, y)
-        elsif @grid[y][x] == 2
-          draw_player2(x, y)
-        end
-      end
-    end
-  end
-
-  def top_left_grid_x
+    def top_left_grid_x
     @center_x-@half_line
   end
 
@@ -57,23 +38,46 @@ class TicTacToe < Gosu::Window
   def draw_player1(x, y)
     center_x = square_center_x(x)
     center_y = square_center_y(y)
-    draw_rect(center_x-30, center_y-30, 60, 60, Gosu::Color::BLACK)
+    Gosu::draw_rect(center_x-30, center_y-30, 60, 60, Gosu::Color::BLACK)
   end
 
   def draw_player2(x, y)
     center_x = square_center_x(x)
     center_y = square_center_y(y)
-    draw_triangle(center_x, center_y-30, Gosu::Color::BLACK,
+    Gosu::draw_triangle(center_x, center_y-30, Gosu::Color::BLACK,
                   center_x-30, center_y+30, Gosu::Color::BLACK,
                   center_x+30, center_y+30, Gosu::Color::BLACK)
   end
 
-  def draw_grid
-    draw_rect(@center_x-@half_square, @center_y-@half_line, 2, @line, Gosu::Color::BLACK)
-    draw_rect(@center_x+@half_square, @center_y-@half_line, 2, @line, Gosu::Color::BLACK)
+  def draw
+    Gosu::draw_rect(@center_x-@half_square, @center_y-@half_line, 2, @line, Gosu::Color::BLACK)
+    Gosu::draw_rect(@center_x+@half_square, @center_y-@half_line, 2, @line, Gosu::Color::BLACK)
 
-    draw_rect(@center_x-@half_line, @center_y-@half_square, @line, 2, Gosu::Color::BLACK)
-    draw_rect(@center_x-@half_line, @center_y+@half_square, @line, 2, Gosu::Color::BLACK)
+    Gosu::draw_rect(@center_x-@half_line, @center_y-@half_square, @line, 2, Gosu::Color::BLACK)
+    Gosu::draw_rect(@center_x-@half_line, @center_y+@half_square, @line, 2, Gosu::Color::BLACK)
+    
+    @grid.each_index do |y|
+      @grid[y].each_index do |x|
+        if @grid[y][x] == 1
+          draw_player1(x, y)
+        elsif @grid[y][x] == 2
+          draw_player2(x, y)
+        end
+      end
+    end
+  end
+end
+
+class TicTacToe < Gosu::Window
+  def initialize
+    super(800, 600)
+    self.caption = 'Tic Tac Toe'
+    @grid = Grid.new(width, height)
+  end
+
+  def draw
+    draw_rect(0, 0, width, height, Gosu::Color::WHITE)
+    @grid.draw
   end
 end
 
